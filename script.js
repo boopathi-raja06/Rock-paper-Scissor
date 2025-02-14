41bf66a5e295d8781a24d19fd3faec3d0d34f453
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-02-14 19:44:44 +0530

    finished

--- script.js
+++ script.js
@@ -22,22 +22,42 @@ function getComputerChoice()
 }
 function getHumanChoice(){
     let humanchoice=prompt("Enter your choice:(rock,paper or scissor)");
-    return humanchoice;
+    return humanchoice.toLowerCase();
 }
 function playRound(humanchoice,computerchoice) {
     let hc=humanchoice;
     let cc=computerchoice;
 
     if(hc==cc){
-        return "Tie";
+        return "Tie! your choice "+hc +" System choice "+cc;
     }
-    else if((hc="rock") && (cc="paper")||(hc="scissor") && (cc= "paper"))
-    {
-        return "your choice "+hc +"System choice "+cc+"YOu WIN!"
+    else if(((hc=="rock") && (cc=="scissor"))||((hc=="scissor") && (cc=="paper")))
+    {   humanScore++;
+        return "your choice "+hc +" System choice "+cc+" YOu got 1 point!"
     }
     else{
-        return "System Win"
+        computerScore++;
+        return "your choice "+hc +" System choice "+cc+" System got 1 point"
     }
     
 }
-console.log(playRound(getHumanChoice(),getComputerChoice));
+// console.log(playRound(getHumanChoice(),getComputerChoice()));
+function playGame()
+{
+    for(let i=0;i<5;i++)
+    {
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+    console.log("Human Score : "+humanScore);
+    console.log("System score : "+computerScore);
+    if(humanScore>computerScore){
+        console.log("You Win!");
+    }
+    else if(computerScore>humanScore)
+    {
+        console.log("System Win!");
+    }
+    else{
+        console.log("Tie !");
+    }
+}
